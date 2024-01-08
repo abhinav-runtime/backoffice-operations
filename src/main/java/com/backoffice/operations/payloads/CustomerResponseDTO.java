9141aee46082f64a18ef26733735be1b34965145
Abhinav Morghade <abhinav@cookietec.co>
2024-01-08 16:48:41 +0530

    Added changes for customer CRUD API's

---
+++ src/main/java/com/backoffice/operations/payloads/CustomerResponseDTO.java
@@ -0,0 +1,44 @@
+package com.backoffice.operations.payloads;
+
+import java.util.Date;
+
+public class CustomerResponseDTO {
+	
+	private String custId;
+    private String name;
+    private String branch;
+    private String country;
+    private Date dateRegistered;
+    
+	public String getCustId() {
+		return custId;
+	}
+	public void setCustId(String custId) {
+		this.custId = custId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public Date getDateRegistered() {
+		return dateRegistered;
+	}
+	public void setDateRegistered(Date dateRegistered) {
+		this.dateRegistered = dateRegistered;
+	}
+   
+}
