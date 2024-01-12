6aa221f1db7b4b0639a6f1d49d307c7554befb61
Abhinav Morghade <abhinav@cookietec.co>
2024-01-12 19:54:09 +0530

    Added server db details

--- src/main/java/com/backoffice/operations/entity/Post.java
+++ src/main/java/com/backoffice/operations/entity/Post.java
@@ -10,7 +10,6 @@ import java.util.Set;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-
 @Entity
 @Table(
         name = "posts", uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})}
