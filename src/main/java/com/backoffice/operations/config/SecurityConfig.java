bd714f3091da902bbc3dd2eff737a9607a19bb2b
Abhinav Morghade <abhinav@cookietec.co>
2024-01-23 10:09:16 +0530

    Added changes for properties file

--- src/main/java/com/backoffice/operations/config/SecurityConfig.java
+++ src/main/java/com/backoffice/operations/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.backoffice.operations.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -17,6 +19,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.backoffice.operations.security.JwtAuthenticationEntryPoint;
 import com.backoffice.operations.security.JwtAuthenticationFilter;
@@ -79,6 +84,16 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(Arrays.asList("https://18.61.204.189"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+    
 //    @Bean
 //    public UserDetailsService userDetailsService(){
 //        UserDetails ramesh = User.builder()
