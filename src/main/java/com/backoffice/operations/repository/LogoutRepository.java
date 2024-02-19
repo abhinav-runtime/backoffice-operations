54e2ae49f79609a4e20ff2234b3fa0cec1f7221f
AbrahamRuntime <abraham.runtime@gmail.com>
2024-02-19 21:23:33 +0530

    Changes are done

--- src/main/java/com/backoffice/operations/repository/LogoutRepository.java
+++
@@ -1,8 +0,0 @@
-package com.backoffice.operations.repository;
-
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import com.backoffice.operations.entity.LogoutEntity;
-//
-//public interface LogoutRepository extends JpaRepository <LogoutEntity, String>{
-//
-//}
