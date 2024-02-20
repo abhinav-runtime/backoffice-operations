8d6b80968aac7ad040e583d40c45368fb0deadfa
sunit-mal <sunitmal1832002@gmail.com>
2024-02-20 11:20:45 +0530

    BO Login, Register, In individual tab fetch all customer and edit customer. And also create BOLoginLog entity to store User-Agent for security purpose.

--- src/main/java/com/backoffice/operations/repository/CustomerRepository.java
+++ src/main/java/com/backoffice/operations/repository/CustomerRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.backoffice.operations.entity.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, String> {
+	public Customer findByCustId(String custId);
 }
