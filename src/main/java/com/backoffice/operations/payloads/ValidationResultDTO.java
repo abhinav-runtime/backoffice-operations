500ad22fde4ffe1f4d1c29402db94ac0ddf05838
Abhinav Morghade <abhinav@cookietec.co>
2024-02-21 19:57:30 +0530

    Added changes for response in set pin API

--- src/main/java/com/backoffice/operations/payloads/ValidationResultDTO.java
+++ src/main/java/com/backoffice/operations/payloads/ValidationResultDTO.java
@@ -42,14 +42,45 @@ public class ValidationResultDTO {
 
 	public static class Data {
 		private String uniqueKey;
+		private String entityId;
+	    private String kitNo;
+	    private String expiryDate;
+	    private String dob;
 
 		public Data() {
 
 		}
 
-		public Data(String uniqueKey) {
-			super();
-			this.uniqueKey = uniqueKey;
+		public String getEntityId() {
+			return entityId;
+		}
+
+		public void setEntityId(String entityId) {
+			this.entityId = entityId;
+		}
+
+		public String getKitNo() {
+			return kitNo;
+		}
+
+		public void setKitNo(String kitNo) {
+			this.kitNo = kitNo;
+		}
+
+		public String getExpiryDate() {
+			return expiryDate;
+		}
+
+		public void setExpiryDate(String expiryDate) {
+			this.expiryDate = expiryDate;
+		}
+
+		public String getDob() {
+			return dob;
+		}
+
+		public void setDob(String dob) {
+			this.dob = dob;
 		}
 
 		public String getUniqueKey() {
