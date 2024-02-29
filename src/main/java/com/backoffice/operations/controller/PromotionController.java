525b36633784ec6eee1a454cf11bc1be03ba18be
sunit-mal <sunitmal1832002@gmail.com>
2024-02-29 19:19:25 +0530

    PromotionController update done

--- src/main/java/com/backoffice/operations/controller/PromotionController.java
+++ src/main/java/com/backoffice/operations/controller/PromotionController.java
@@ -1,44 +1,62 @@
 package com.backoffice.operations.controller;
 
 import com.backoffice.operations.payloads.PromotionDTO;
+import com.backoffice.operations.payloads.common.GenericResponseDTO;
 import com.backoffice.operations.service.PromotionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/promotions")
 public class PromotionController {
 
-    private final PromotionService promotionService;
+	private final PromotionService promotionService;
 
-    public PromotionController(PromotionService promotionService) {
-        this.promotionService = promotionService;
-    }
+	public PromotionController(PromotionService promotionService) {
+		this.promotionService = promotionService;
+	}
 
-    @PostMapping
-    public ResponseEntity<String> createPromotion(@RequestBody PromotionDTO promotionDTO) {
-        PromotionDTO savedPromotion = promotionService.savePromotion(promotionDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body("Promotion created with ID: " + savedPromotion.getId());
-    }
+	@PostMapping
+	public ResponseEntity<Object> createPromotion(@RequestBody PromotionDTO promotionDTO) {
+		GenericResponseDTO<Object> response = new GenericResponseDTO<>();
+		PromotionDTO savedPromotion = promotionService.savePromotion(promotionDTO);
+		response.setStatus("Success");
+		response.setMessage("Promotion created with ID : " + savedPromotion.getId());
+		response.setData(new HashMap<>());
+		return new ResponseEntity<>(response, HttpStatus.CREATED);
+	}
 
-    @GetMapping
-    public ResponseEntity<List<PromotionDTO>> getAllPromotions() {
-        List<PromotionDTO> promotions = promotionService.getAllPromotions();
-        return ResponseEntity.ok(promotions);
-    }
+	@GetMapping
+	public ResponseEntity<Object> getAllPromotions() {
+		GenericResponseDTO<Object> response = new GenericResponseDTO<>();
+		List<PromotionDTO> promotions = promotionService.getAllPromotions();
+		response.setStatus("Success");
+		response.setMessage("Promotions fetched successfully");
+		response.setData(promotions);
+		return ResponseEntity.ok(response);
+	}
 
-    @GetMapping("/{id}")
-    public ResponseEntity<PromotionDTO> getPromotionById(@PathVariable String id) {
-        PromotionDTO promotion = promotionService.getPromotionById(id);
-        return ResponseEntity.ok(promotion);
-    }
+	@GetMapping("/{id}")
+	public ResponseEntity<Object> getPromotionById(@PathVariable String id) {
+		GenericResponseDTO<Object> response = new GenericResponseDTO<>();
+		PromotionDTO promotion = promotionService.getPromotionById(id);
+		response.setStatus("Success");
+		response.setMessage("Promotion fetched successfully");
+		response.setData(promotion);
+		return ResponseEntity.ok(response);
+	}
 
-    @PutMapping("/{id}")
-    public ResponseEntity<String> markPromotionAsSeen(@PathVariable String id) {
-        promotionService.markPromotionAsSeen(id);
-        return ResponseEntity.ok("Promotion with ID " + id + " marked as seen.");
-    }
+	@PutMapping("/{id}")
+	public ResponseEntity<Object> markPromotionAsSeen(@PathVariable String id) {
+		GenericResponseDTO<Object> response = new GenericResponseDTO<>();
+		promotionService.markPromotionAsSeen(id);
+		response.setStatus("Success");
+		response.setMessage("Promotion with ID " + id + " marked as seen.");
+		response.setData(new HashMap<>());
+		return ResponseEntity.ok(response);
+	}
 }
