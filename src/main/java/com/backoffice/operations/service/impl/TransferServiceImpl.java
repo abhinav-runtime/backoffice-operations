0e3db29d9e3c9326d4dc86dfec353ac3808e6cca
shreya <shreya1.runtime@gmail.com>
2024-02-29 22:32:22 +0530

    removed hard coded hearder

--- src/main/java/com/backoffice/operations/service/impl/TransferServiceImpl.java
+++ src/main/java/com/backoffice/operations/service/impl/TransferServiceImpl.java
@@ -43,11 +43,6 @@ public class TransferServiceImpl implements TransferService {
         GenericResponseDTO<Object> responseDTO = new GenericResponseDTO<>();
             HttpHeaders headers = new HttpHeaders();
             headers.setBearerAuth(response.getBody().getAccessToken());
-            TransferRequestDto.Header header = new TransferRequestDto.Header();
-            header.setSource_system("mpp-digital-app");
-            header.setSource_user("Aditya");
-            header.setSource_operation("Fund Transfer");
-            transferRequest.setHeader(header);
             ObjectMapper objectMapper = JsonMapper.builder()
                     .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
                     .build();
