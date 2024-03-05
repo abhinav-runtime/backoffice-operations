e7eaf7ad7544248b67b58689eeed623124d480be
sunit-mal <sunitmal1832002@gmail.com>
2024-03-05 23:52:10 +0530

    Fetch Preference amd get preference for card module implement successfully complete.

--- src/main/java/com/backoffice/operations/controller/BoCardController.java
+++ src/main/java/com/backoffice/operations/controller/BoCardController.java
@@ -5,11 +5,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.backoffice.operations.payloads.common.GenericResponseDTO;
 import com.backoffice.operations.service.BoCarddetailService;
+import com.fasterxml.jackson.databind.JsonNode;
 
 @RestController
 @RequestMapping("/bo/v1/card")
@@ -23,6 +26,26 @@ public class BoCardController {
 		if (responseDTO.getStatus().equals("Failure")) {
 			return new ResponseEntity<>(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
 		}else {
+			return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+		}
+	}
+	
+	@GetMapping("/preference/{custNo}")
+	public ResponseEntity<Object> fetchPreference(@PathVariable String custNo) {
+		GenericResponseDTO<Object> responseDTO = boCardDetailService.fetchPreference(custNo);
+		if (responseDTO.getStatus().equals("Failure")) {
+			return new ResponseEntity<>(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+		} else {
+			return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+		}
+	}
+	
+	@PostMapping("/preference")
+	public ResponseEntity<Object> setPreference(@RequestBody JsonNode requestBody) {
+		GenericResponseDTO<Object> responseDTO = boCardDetailService.setPreference(requestBody);
+		if (responseDTO.getStatus().equals("Failure")) {
+			return new ResponseEntity<>(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+		} else {
 			return new ResponseEntity<>(responseDTO, HttpStatus.OK);
 		}
 	}
--- src/main/java/com/backoffice/operations/service/BoCarddetailService.java
+++ src/main/java/com/backoffice/operations/service/BoCarddetailService.java
@@ -1,7 +1,10 @@
 package com.backoffice.operations.service;
 
 import com.backoffice.operations.payloads.common.GenericResponseDTO;
+import com.fasterxml.jackson.databind.JsonNode;
 
 public interface BoCarddetailService {
 	GenericResponseDTO<Object> fetchCardDeatils(String custNo);
+	GenericResponseDTO<Object> fetchPreference(String custNo);
+	GenericResponseDTO<Object> setPreference(JsonNode preference);
 }
