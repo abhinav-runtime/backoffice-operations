4877dade987e1f6fad3fcaf98a85a729e0393ca1
shreya <shreya1.runtime@gmail.com>
2024-03-06 19:14:36 +0530

    Added blocked amount api

--- src/main/java/com/backoffice/operations/controller/DashboardController.java
+++ src/main/java/com/backoffice/operations/controller/DashboardController.java
@@ -132,4 +132,9 @@ public class DashboardController {
     public ResponseEntity<GenericResponseDTO<Object>> editInfo(@RequestBody EditInfoRequestDto editInfoRequestDto){
         return ResponseEntity.ok(dashboardService.editInfo(editInfoRequestDto));
     }
+
+    @PostMapping("/blockedAmounts")
+    public ResponseEntity<GenericResponseDTO<Object>> getBlockedAmounts(@RequestParam String uniqueKey){
+        return ResponseEntity.ok(dashboardService.getBlockedAmounts(uniqueKey));
+    }
 }
