bfefac72e78293e57a01e857c6f7cee9bd440ac8
shreya <shreya1.runtime@gmail.com>
2024-03-14 22:56:51 +0530

    changes added for transfer api & self transfer & dashboard api changes

--- src/main/java/com/backoffice/operations/entity/DashboardEntity.java
+++ src/main/java/com/backoffice/operations/entity/DashboardEntity.java
@@ -37,4 +37,10 @@ public class DashboardEntity {
     private double lowBalLimit;
     private String customerNickName;
     private String uniqueKey;
+    private String requestDebitCard;
+    private String requestsChequeBook;
+    private String billPayments;
+    private String transfers;
+    private String editAccountInfo;
+    private String visibility;
 }
