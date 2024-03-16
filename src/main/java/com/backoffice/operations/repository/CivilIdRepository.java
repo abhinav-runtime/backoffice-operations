358269555cb27d6d120f0a80fa020cfada5481c9
sunit-mal <sunitmal1832002@gmail.com>
2024-03-16 22:31:56 +0530

    Update ValidationServiceImpl on line no 61-63 and 126 -135

--- src/main/java/com/backoffice/operations/repository/CivilIdRepository.java
+++ src/main/java/com/backoffice/operations/repository/CivilIdRepository.java
@@ -4,9 +4,11 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.backoffice.operations.entity.CivilIdEntity;
+import java.util.List;
+
 
 public interface CivilIdRepository extends JpaRepository<CivilIdEntity, String> {
 
 	Optional<CivilIdEntity> findByEntityId(String entityId);
-
+	List<CivilIdEntity> findByCivilId(String civilId);
 }
